b7894d4e5fb1e3b439218b74517ce308102044c3
Justin Kilpatrick <kilpatrickjustin@protonmail.ch>
2018-05-03 10:43:04 -0400

    Add upgrade mode
    also do some autopep8

--- flash-althea.py
+++ flash-althea.py
@@ -1,6 +1,6 @@
 """
 Usage:
-  autoflash flash [--device=<id>] [--loop] [--tmpdir=<dir>] [--router-ip=<ip>] [-v] [--vv]
+  autoflash flash [--device=<id>] [--loop] [--tmpdir=<dir>] [--router-ip=<ip>] [-u] [-v] [--vv]
   autoflash list-devices [-v] [--vv]
   autoflash download --device=<id> [--tmpdir=<dir>] [-v] [--vv]
 
@@ -9,6 +9,7 @@ Options:
   --loop                 Repeat the flash on a loop to flash many identical devices
   --tmpdir=<dir>         Filepath for the storage of Althea firmware files [default: Current Directory]
   --router-ip=<ip>       SSH ip address for devices like the aclite that dhcp out of the box
+  -u                     Update, this device is already running and needs a update
   -v                     Application level debugging
   -vv                    Library level debugging
 """
@@ -20,7 +21,7 @@ import docopt
 import logging
 import json
 
-firmware_url="https://updates.altheamesh.com/"
+firmware_url = "https://updates.altheamesh.com/"
 
 
 # Generates a dictionary of firmware tags to urls
@@ -38,10 +39,12 @@ def get_devices_list():
 
     return devices
 
+
 def display_devices_list():
     devices = get_devices_list()
     for entry in devices:
         print(entry)
+
 
 def download(tmpdir, device):
     filename = device['firmwareURL'].split("/")[-1]
@@ -56,11 +59,14 @@ def download(tmpdir, device):
     return tmpdir + filename, filename
 
 # To handle devices that need jailbreak firmware
+
+
 def download_intermediary_firmware(tmpdir, device):
     filename = device['intermediaryFirmwareURL'].split("/")[-1]
     url = device['intermediaryFirmwareURL']
 
-    logging.info("Downloading Intermediary firmware file " + filename + " to " + tmpdir)
+    logging.info("Downloading Intermediary firmware file " +
+                 filename + " to " + tmpdir)
 
     with open(tmpdir + filename, "wb") as file:
         response = requests.get(url, timeout=10)
@@ -71,24 +77,27 @@ def download_intermediary_firmware(tmpdir, device):
 
 # Takes a device populated with settings, returns a tuple of sftp and ssh connections
 def ssh_setup(device):
-    logging.info("ssh {}@{}:{}".format(device['sshUser'],device['sshIP'],device['sshPort']))
+    logging.info("ssh {}@{}:{}".format(
+        device['sshUser'], device['sshIP'], device['sshPort']))
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     while True:
         try:
             client.connect(device['sshIP'],
-                        port=device['sshPort'],
-                        username=device['sshUser'],
-                        password=device['sshPassword'],
-                        look_for_keys=False)
+                           port=device['sshPort'],
+                           username=device['sshUser'],
+                           password=device['sshPassword'],
+                           look_for_keys=False)
         except paramiko.ssh_exception.NoValidConnectionsError:
             print("Can't find router via ssh, retrying. ")
-            print("If this happens for more than 5 minutes, double check router power and connection")
+            print(
+                "If this happens for more than 5 minutes, double check router power and connection")
             sleep(30)
         break
     sftp = client.open_sftp()
 
     return client, sftp
+
 
 def run_ssh_command(client, command):
     logging.info("Running command: {}".format(command))
@@ -105,33 +114,38 @@ def run_ssh_command(client, command):
 
 # Flash method for devices that already run OpenWrt of some kind.
 def flash_sysupgrade(device):
-
+    input("Plug into the Althea devices LAN port (physically furthest from the lan port) then press enter")
     client, sftp = ssh_setup(device)
 
     firmware_location = "/tmp/" + device['firmwareFilename']
     sftp.put(device['firmwarePath'], firmware_location)
 
-    stdout, stderr, rc = client.exec_command("sysupgrade -v -n" + firmware_location)
+    stdout, stderr, rc = client.exec_command(
+        "sysupgrade -v -n" + firmware_location)
     print(stdout)
     print(stderr)
 
 # Specialied flashing routine for edgerouterlite
+
+
 def flash_edgerouterlite(device):
     print("TODO")
 
 # Specialized flashing routine for edgerouterx
+
+
 def flash_edgerouterx(device, opts):
     intermediary_firmware_path, intermediary_firmware_filename = \
-    download_intermediary_firmware(opts['--tmpdir'], device)
+        download_intermediary_firmware(opts['--tmpdir'], device)
 
     input("Please plug your computer into eth0 of the EdgerouterX then press enter\n")
     client, sftp = ssh_setup(device)
     env_magic = "/opt/vyatta/bin/vyatta-op-cmd-wrapper"
 
-
     intermediary_firmware_location = "/tmp/" + intermediary_firmware_filename
     sftp.put(intermediary_firmware_path, intermediary_firmware_location)
-    run_ssh_command(client, "{} add system image {}".format(env_magic ,intermediary_firmware_location))
+    run_ssh_command(client, "{} add system image {}".format(
+        env_magic, intermediary_firmware_location))
     run_ssh_command(client, "{} reboot now".format(env_magic))
 
     print("Waiting for EdgerouterX to perform jailbreak reboot, do not unplug")
@@ -156,6 +170,7 @@ def flash_edgerouterx(device, opts):
     sftp.close()
     print("Flashing Successful!")
 
+
 def flash_aclite(device, opts):
     input("Please plug both your computer and the ACLite into a network using\
 the same dhcp service. Find it's ip either by browsing your routers device page\
@@ -165,10 +180,16 @@ or using some external program like nmap. Press enter when ready")
         opts['--device-ip'] = input("Please enter the device ip")
 
 
-
-
 def flash_device(device, opts):
-    if device['flashMethod'] == "sysupgrade":
+    if opts['-u']:
+        # Upgrade is a special case of the sysupgrade code where we can
+        # use the default values from the firmware.
+        device['sshIP'] = "192.168.1.1"
+        device['sshPort'] = "22"
+        device['sshUser'] = "root"
+        device['sshPassword'] = ""
+        flash_sysupgrade(device)
+    elif device['flashMethod'] == "sysupgrade":
         flash_sysupgrade(device)
     elif device['flashMethod'] == "edgerouterlite":
         flash_edgerouterlite(device)
@@ -177,8 +198,10 @@ def flash_device(device, opts):
     elif device['flashMethod'] == "aclite":
         flash_aclite(device, opts)
     else:
-        logging.error("Flashing method for this device is not supported, please update this program")
+        logging.error(
+            "Flashing method for this device is not supported, please update this program")
         exit(1)
+
 
 def main():
     opts = docopt.docopt(__doc__)
@@ -191,10 +214,9 @@ please make sure your computer has internet access until instructed to connect d
         logging.basicConfig(level=logging.DEBUG)
 
     if opts['--tmpdir'] == 'Current Directory':
-        tmpdir = "./" #TODO platform detection
+        tmpdir = "./"  # TODO platform detection
     else:
         tmpdir = opts['--tmpdir']
-
 
     if opts['list-devices']:
         display_devices_list()
@@ -205,12 +227,14 @@ please make sure your computer has internet access until instructed to connect d
         devices = get_devices_list()
         device = devices[opts['--device']]
 
-        device['firmwarePath'], device['firmwareFilename'] = download(tmpdir, device)
+        device['firmwarePath'], device['firmwareFilename'] = download(
+            tmpdir, device)
 
         if opts['--loop']:
             while True:
                 flash_device(device, opts)
         else:
             flash_device(device, opts)
+
 
 main()
